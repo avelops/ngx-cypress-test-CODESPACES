93ba4f4dfb34d225deb42d9026c4190ee7419390
avelops <49043342+avelops@users.noreply.github.com>
2024-03-14 21:20:10 +0000

    fix yarn error with github:

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -2,41 +2,67 @@
 
 describe('First Test Suite', () => {
 
-  it('first test',() => {
-      
-    
-      cy.visit('/')
-      cy.contains('Forms').click()
-      cy.contains('Form Layouts').click()
-    
-    
-      // by tag name
-      cy.get('input')
+  it('first test', () => {
 
-      // by id
-      cy.get('#inputEmail1')
 
-      // by class
-      cy.get('.input-full-width')
+    cy.visit('/')
+    cy.contains('Forms').click()
+    cy.contains('Form Layouts').click()
 
-      // by attribute
-      cy.get('[fullwidth]')
 
-      //by attribute and value
-      cy.get('[placeholder="Email"]')
+    // by tag name
+    cy.get('input')
 
-      // by entire class value
-      cy.get('[class="input-full-width size-medium shape-rectangle"]')
-  
-      // by two attributes
-      cy.get('[placeholder="Email"][fullwidth]')
+    // by id
+    cy.get('#inputEmail1')
 
-      // by tag, attribute, id and class
-      cy.get('input[placeholder="Email"]#inputEmail1.input-full-width')
+    // by class
+    cy.get('.input-full-width')
 
-      // by cypress test ID
-      cy.get('[data-cy="imputEmail1"]')
+    // by attribute
+    cy.get('[fullwidth]')
 
-    })
-  
+    //by attribute and value
+    cy.get('[placeholder="Email"]')
+
+    // by entire class value
+    cy.get('[class="input-full-width size-medium shape-rectangle"]')
+
+    // by two attributes
+    cy.get('[placeholder="Email"][fullwidth]')
+
+    // by tag, attribute, id and class
+    cy.get('input[placeholder="Email"]#inputEmail1.input-full-width')
+
+    // by cypress test ID
+    cy.get('[data-cy="imputEmail1"]')
+
+  })
+
+  it('second test', () => {
+
+
+    cy.visit('/')
+    cy.contains('Forms').click()
+    cy.contains('Form Layouts').click()
+    cy.contains('Sign in')
+    cy.contains('[status="warning"]', 'Sign in')
+    cy.contains('nb-card', 'Horizontal form').find('button')
+    cy.contains('nb-card', 'Horizontal form').contains('Sign in')
+  })
+
+
+  it.only('third test', () => {
+
+    cy.visit('/')
+    cy.contains('Forms').click()
+    cy.contains('Form Layouts').click()
+    cy.get('#exampleInputEmail1')
+      .parents('form')
+      .find('button')
+      .should('contain','Submit')
+      .parents('form')
+      .find('.custom-checkbox')
+
+  })
 })
