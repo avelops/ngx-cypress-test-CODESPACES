72b6cb825272158114fe5a2f4bca1ed568c979bd
avelops <49043342+avelops@users.noreply.github.com>
2024-03-28 16:40:46 +0000

    tooltip

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -1,5 +1,4 @@
 /// <reference types="cypress" />
-
 describe('First Test Suite', () => {
 
   it('first test', () => {
@@ -52,7 +51,7 @@ describe('First Test Suite', () => {
   })
 
 
-  it.only('third test', () => {
+  it('third test', () => {
 
     cy.visit('/')
     cy.contains('Forms').click()
@@ -60,9 +59,224 @@ describe('First Test Suite', () => {
     cy.get('#exampleInputEmail1')
       .parents('form')
       .find('button')
-      .should('contain','Submit')
+      .should('contain', 'Submit')
       .parents('form')
       .find('.custom-checkbox')
+      .click()
 
   })
+
+  it('save subject', () => {
+
+    cy.visit('/')
+    cy.contains('Forms').click()
+    cy.contains('Form Layouts').click()
+
+    cy.contains('nb-card', 'Using the Grid').as('usingTheGrid')
+    cy.get('@usingTheGrid').find('[for="inputEmail1"]').should('contain', 'Email')
+    cy.get('@usingTheGrid').find('[for="inputPassword2"]').should('contain', 'Password')
+
+
+    cy.contains('nb-card', 'Using the Grid').then(usingTheGridForm => {
+      cy.wrap(usingTheGridForm).find('[for="inputEmail1"]').should('contain', 'Email')
+      cy.wrap(usingTheGridForm).find('[for="inputPassword2"]').should('contain', 'Password')
+    })
+
+  })
+
+
+  it('Extract Text', () => {
+
+    cy.visit('/')
+    cy.contains('Forms').click()
+    cy.contains('Form Layouts').click()
+
+    cy.get('[for="exampleInputEmail1"]').then(label => {
+      const labelText = label.text()
+      expect(labelText).to.equal('Email address')
+    })
+
+
+    cy.get('[for="exampleInputEmail1"]').invoke('text').then(text => {
+      expect(text).to.equal('Email address')
+    })
+
+
+    cy.get('[for="exampleInputEmail1"]').invoke('text').as('labelText').should('contain', 'Email address')
+
+
+    cy.get('@labelText').should('contain', 'Email address')
+
+
+    cy.get('[for="exampleInputEmail1"]').invoke('attr', 'class').then(classValue => {
+      expect(classValue).to.equal('label')
+    })
+
+
+    cy.get('#exampleInputEmail1').type('test@test.com')
+
+    cy.get('#exampleInputEmail1').invoke('prop', 'value').should('contain', 'test@test.com').then(property => {
+      expect(property).to.equal('test@test.com')
+    })
+  })
+
+  it('Radio button', () => {
+
+    cy.visit('/')
+    cy.contains('Forms').click()
+    cy.contains('Form Layouts').click()
+
+    //using force flag when input is visually hidden
+
+    cy.contains('nb-card', 'Using the Grid').find('[type="radio"]').then(radioButtons => {
+      cy.wrap(radioButtons).eq(0).check({ force: true }).should('be.checked')
+      cy.wrap(radioButtons).eq(1).check({ force: true })
+      cy.wrap(radioButtons).eq(0).should('not.be.checked')
+      cy.wrap(radioButtons).eq(2).should('be.disabled')
+
+
+
+
+
+    })
+  })
+  it('check boxes ', () => {
+
+    cy.visit('/')
+    cy.contains('Modal & Overlays').click()
+    cy.contains('Toastr').click()
+
+    //using force flag when input is visually hidden
+
+    cy.get('[type="checkbox"]').eq(1).check({ force: true })
+  })
+
+  it('date picker', () => {
+
+    // recursive function to navigate to future dates in date picker
+    function selectDayFromCurrent(day) {
+      let date = new Date()
+      date.setDate(date.getDate() + day)
+      let futureDay = date.getDate()
+      let futureMonth = date.toLocaleDateString('en-Us', { month: "short" })
+      let futureYear = date.getFullYear()
+      let dateToAssert = `${futureMonth} ${futureDay}, ${futureYear}`
+
+      cy.get('nb-calendar-navigation').invoke('attr', 'ng-reflect-date').then((dateAttribute) => {
+        if (!dateAttribute.includes(futureMonth) || !dateAttribute.includes(futureYear)) {
+          cy.get('[data-name="chevron-right"]').click()
+          selectDayFromCurrent(day)
+        }
+        else {
+          cy.get('.day-cell').not('.bounding-month').contains(futureDay).click()
+        }
+      })
+      return dateToAssert
+    }
+
+
+    cy.visit('/')
+    cy.contains('Forms').click()
+    cy.contains('Datepicker').click()
+
+
+
+    cy.contains('nb-card', 'Common Datepicker').find('input').then(input => {
+      cy.wrap(input).click()
+
+      const dateToAssert = selectDayFromCurrent(7)
+      cy.wrap(input).invoke('prop', 'value').should('contain', dateToAssert)
+    })
+
+  })
+
+  it('lists and drop downs ', () => {
+
+    cy.visit('/')
+
+    //1
+    cy.get('nav nb-select').click()
+    cy.get('.options-list').contains('Dark').click()
+    cy.get('nav nb-select').should('contain', 'Dark')
+
+    //2 iterate a list using a each loop
+    cy.get('nav nb-select').then((dropDown) => {
+      cy.wrap(dropDown).click()
+      cy.get('.options-list nb-option').each((listItem, index) => {
+        const itemText = listItem.text().trim()
+        cy.wrap(listItem).click()
+        cy.wrap(dropDown).should('contain', itemText)
+        if (index < 3) {
+          cy.wrap(dropDown).click()
+        }
+      })
+    })
+  })
+
+  it('tables ', () => {
+
+    cy.visit('/')
+    cy.contains('Tables & Data').click()
+    cy.contains('Smart Table').click()
+    // // get row by text
+    // cy.get('tbody').contains('tr', 'Larry').then((tableRow) => {
+    //   cy.wrap(tableRow).find('.nb-edit').click()
+    //   cy.wrap(tableRow).find('[placeholder="Age"]').clear().type('25')
+    //   cy.wrap(tableRow).find('.nb-checkmark').click()
+    //   cy.wrap(tableRow).find('td').eq(6).should('contain','25')
+    // })
+
+
+
+    // // get row by index
+    // cy.get('thead .nb-plus').click()
+    // cy.get('thead tr').eq(2).then((tableRow) => {
+    //   cy.wrap(tableRow).find('[placeholder="First Name"]').type('John')
+    //   cy.wrap(tableRow).find('[placeholder="Last Name"]').type('Smith')
+    //   cy.wrap(tableRow).find('.nb-checkmark').click()
+    // })
+
+    // cy.get('tbody tr').eq(0).find('td').then((tableColumn) => {
+    //   cy.wrap(tableColumn).eq(2).should('contain', 'John')
+    //   cy.wrap(tableColumn).eq(3).should('contain', 'Smith')
+    // })
+
+    const age = [20, 30, 40, 200]
+
+    cy.wrap(age).each((age) => {
+      // get each row validation
+      cy.get('thead [placeholder="Age"]').clear().type(age)
+      cy.wait(500)
+      cy.get('tbody tr').each((tableRow) => {
+
+        if (age === 200) {
+          cy.wrap(tableRow).should('contain', 'No data found')
+        }
+        else {
+          cy.wrap(tableRow).find('td').eq(6).should('contain', age)
+        }
+
+      })
+    })
+
+
+
+
+
+
+  })
+
+  it.only('Tooltips',() => {
+    cy.visit('/')
+    cy.contains('Modal & Overlays').click()
+    cy.contains('Tooltip').click()
+    cy.contains('nb-card', 'Colored Tooltips')
+      .contains('Default').click()
+    cy.get('nb-tooltip').should('contain','This is a tooltip')
+
+
+
+  }
+
+  )
 })
